86bb2343bb8e346cc3380ee9ea36d7e3e3ce63b5
rovenelabanga <rovenelabanga2001@gmail.com>
2024-09-27 21:32:42 +0300

    uses loops

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const myArray = ["Guadalupe", "Ollie", "Aki"];
+const writeCards = function(array,surprise){
+    const messages = [];
+
+    for(let i=0; i<array.length; i++){
+        messages.push( `Thank you, ${array[i]}, for the wonderful ${surprise} gift!`);
+    }
+
+    return messages;
+}
+console.log(writeCards(myArray, "surprise"));
+
+const countDown = function(){
+    for(let i=0; i<11; i++){
+        console.log(i)
+    }
+}
